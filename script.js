1b6130a2e0b0099b86785abe1d06383fdea0cf38
Obianuju-Sunday <obianujusunday43@gmail.com>
2023-08-18 19:25:47 +0100

    budget--app

---
+++ script.js
@@ -0,0 +1,73 @@
+// BUDGET CONTROLLER
+
+var budgetController = (function () {
+
+    // Code goes here
+
+
+})();
+
+
+// UI CONTROLLER
+var UIController = (function () {
+
+    // Code goes here
+
+})
+
+
+// GLOBAL APP CONTROLLER
+var AppController = (function(budgetCtrl, UICtrl) {
+
+document.querySelector('.add__btn').addEventListener('click', function(){
+
+
+    // 1. Get the field input data
+
+    // 2. Add the item to the budget controller
+
+    // 3. Add the item to the UI 
+
+    // 4. Calculate the budget
+
+    // 5. Display the budget on the UI
+})
+
+})(budgetController, UIController);
+
+
+// https://us05web.zoom.us/j/88338607232?pwd=bbYcIKMWUvNxiTekNi416PpLOUXdHi.1 //
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
