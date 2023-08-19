8113f7ade05144e78a3bca71f90341a52ab39497
Obianuju-Sunday <obianujusunday43@gmail.com>
2023-08-19 20:55:46 +0100

    Event listener added

--- script.js
+++ script.js
@@ -11,32 +11,53 @@ var budgetController = (function () {
 // UI CONTROLLER
 var UIController = (function () {
 
-    // Code goes here
+    return {
+        getinput: function () {
+
+            // In order to return three properties same time , we have to make it an object.
+            
+            return {
+                type: document.querySelector('.add__type').value, // Will be either inc or exp
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            }
+        }
+    }
 
 })
 
 
 // GLOBAL APP CONTROLLER
-var AppController = (function(budgetCtrl, UICtrl) {
+var AppController = (function (budgetCtrl, UICtrl) {
 
-document.querySelector('.add__btn').addEventListener('click', function(){
+    var ctrlAddItem = function () {
+        // 1. Get the field input data
 
+        // 2. Add the item to the budget controller
 
-    // 1. Get the field input data
+        // 3. Add the item to the UI 
 
-    // 2. Add the item to the budget controller
+        // 4. Calculate the budget
 
-    // 3. Add the item to the UI 
+        // 5. Display the budget on the UI
 
-    // 4. Calculate the budget
+        console.log('It works');
+    }
 
-    // 5. Display the budget on the UI
-})
+    document.querySelector('.add__btn').addEventListener('click', ctrlAddItem)
+
+    document.addEventListener('keypress', function (event) {
+
+        // Some older browsers don't use the 'keyCode' property here. They use another property called 'which'.
+
+        if (event.keyCode === 13 || event.which === 13) {
+            ctrlAddItem();
+        }
+    })
 
 })(budgetController, UIController);
 
 
-// https://us05web.zoom.us/j/88338607232?pwd=bbYcIKMWUvNxiTekNi416PpLOUXdHi.1 //
 
 
 
